c58dba0eb9ec6366f01b40ae2a4e7793a115e56b
Marc Görtz <mail@marcgoertz.de>
2021-11-08 18:50:43 +0100

    Refactor tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,41 +7,26 @@ const getFixturePath = name => `tests/fixtures/${name}.css`;
 
 const readFixture = async name => fs.promises.readFile(getFixturePath(name), 'utf8');
 
-const testFixture = async (name, pluginOptions = {}, postcssOptions = {}) => {
+const testFixture = async (t, name, pluginOptions = {}, postcssOptions = {}) => {
 	postcssOptions.from = getFixturePath(name);
-	const test = await readFixture(name);
+	const actual = await readFixture(name);
 	const expected = await readFixture(`${name}-expected`);
-	const result = await postcss([plugin(pluginOptions)]).process(test, postcssOptions);
+	const result = await postcss([plugin(pluginOptions)]).process(actual, postcssOptions);
 
-	return {expected, result};
+	t.is(result.warnings().length, 0);
+	t.deepEqual(result.css, expected);
 };
 
-test('Transforms opacity with a percentage-based value', async t => {
-	const {expected, result} = await testFixture('opacity-percentage');
-	t.is(result.warnings().length, 0);
-	t.deepEqual(result.css, expected);
-});
+const tests = {
+	'opacity-percentage': 'Transforms opacity with a percentage-based value',
+	'opacity-float': 'Does not transform opacity with a floating point value',
+	'opacity-custom-property': 'Opacity value with custom property keeps unchanged',
+	'do-nothing-value': 'Textual opacity value keeps unchanged',
+	'empty-value': 'Empty opacity value does not stop the plugin',
+};
 
-test('Does not transform opacity with a floating point value', async t => {
-	const {expected, result} = await testFixture('opacity-float');
-	t.is(result.warnings().length, 0);
-	t.deepEqual(result.css, expected);
-});
-
-test('Opacity value with custom property keeps unchanged', async t => {
-	const {expected, result} = await testFixture('opacity-custom-property');
-	t.is(result.warnings().length, 0);
-	t.deepEqual(result.css, expected);
-});
-
-test('Textual opacity value keeps unchanged', async t => {
-	const {expected, result} = await testFixture('do-nothing-value');
-	t.is(result.warnings().length, 0);
-	t.deepEqual(result.css, expected);
-});
-
-test('Empty opacity value does not stop the plugin', async t => {
-	const {expected, result} = await testFixture('empty-value');
-	t.is(result.warnings().length, 0);
-	t.deepEqual(result.css, expected);
-});
+for (const [fixture, description] of Object.entries(tests)) {
+	test(description, async t => {
+		await testFixture(t, fixture);
+	});
+}
